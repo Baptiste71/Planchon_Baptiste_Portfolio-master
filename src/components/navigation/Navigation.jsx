8159ce68510bd9b85ed1290b9ea6189a71d9887c
Baptiste71 <planchonbaptiste@gmail.com>
2022-07-01 11:08:27 +0200

    responsive page d'accueil - ok

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,25 +1,45 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./_navigation.scss";
+import HomeIcon from "@mui/icons-material/Home";
+import WorkIcon from "@mui/icons-material/Work";
+import SchoolIcon from "@mui/icons-material/School";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Navigation = () => {
   return (
     <div className="navigation">
       <ul className="direction">
         <NavLink className="home" to="/home">
-          <li>Accueil</li>
+          <span className="mobileIcon">
+            <HomeIcon />
+          </span>
+          <li className="navDesktop">Accueil</li>
         </NavLink>
         <NavLink className="projects" to="/projects">
-          <li>Mes projet</li>
+          <span className="mobileIcon">
+            <WorkIcon />
+          </span>
+          <li className="navDesktop">Mes projets</li>
         </NavLink>
         <NavLink className="formation" to="/formation">
-          <li>Ma formation</li>
+          <span className="mobileIcon">
+            <SchoolIcon />
+          </span>
+          <li className="navDesktop">Ma formation</li>
         </NavLink>
         <NavLink className="videoPresentation" to="/videopresentation">
-          <li>Presentation vidéo</li>
+          <span className="mobileIcon">
+            <VideoLibraryIcon />
+          </span>
+          <li className="navDesktop">Presentation vidéo</li>
         </NavLink>
         <NavLink className="contact" to="/contact">
-          <li>Contact</li>
+          <span className="mobileIcon">
+            <EmailIcon />
+          </span>
+          <li className="navDesktop">Contact</li>
         </NavLink>
       </ul>
     </div>
